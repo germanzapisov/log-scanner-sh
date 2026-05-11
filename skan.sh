49e696c22acce8dad54ff68bbb00328c09049538
Герман <german@MacBook-Air-German.local>
2026-05-11 21:23:30 +0300

    skanner photo added

--- skan.sh
+++ skan.sh
@@ -2,14 +2,24 @@
 cd ~/Desktop
 mkdir logs
 cd logs
+mkdir photo
 echo > logs_skan.txt
 
-function skanner() {
+function skanner_debug() {
   find ~ -name "$1" | while read file
 do
   grep -EH 'ERROR|DEBUG' "$file" >> logs_skan.txt
 done
 }
 
-skanner "*.log"
+function skanner_photo() {
+  find ~ -type f -name "$1"| while read photo
+  do
+    cp "$photo" ~/Desktop/logs/photo
+  done
+}
+skanner_debug "*.txt"
+
+skanner_photo "*.jpg"
+
 echo "successful"
