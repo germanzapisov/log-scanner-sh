bf78344e628ec7865caf8a149e09bb35bf6bd27c
Герман <german@MacBook-Air-German.local>
2026-05-11 21:03:43 +0300

    function added

--- skan.sh
+++ skan.sh
@@ -1,11 +1,15 @@
-cd ~
+
 cd ~/Desktop
 mkdir logs
 cd logs
-echo '' > logs_skan.txt
+echo > logs_skan.txt
 
-find ~ -name '*.log'| while read file
+function skanner() {
+  find ~ -name "$1" | while read file
 do
-  grep -H 'ERROR' '$file' >> logs_skan.txt
-  grep -H 'DEBUG' '$file' >> logs_skan.txt
+  grep -EH 'ERROR|DEBUG' "$file" >> logs_skan.txt
 done
+}
+
+skanner "*.log"
+echo "successful"
